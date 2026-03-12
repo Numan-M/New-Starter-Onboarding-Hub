a6d7393ef14037092a6b015deee9ef30d566a8ab
Numan-M <Numan_mahmood@hotmail.co.uk>
2026-03-12 13:58:14 +0000

    Added Docstrings

--- app/routes.py
+++ app/routes.py
@@ -3,5 +3,6 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def Index():
+    """This function defines the route for the index page of the Flask application."""
     return "Hello, World!"
 
